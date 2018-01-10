e4dbf5b73f3896d8f25b46a8ba525d2b1fde90a0
Ray Thomas (stash) <Ray.Thomas@charter.com>
2018-01-10 12:38:14 -0700

    converted tabs to spaces

--- throttlesym.app/Contents/Resources/throttlesym/bin/throttlesym.sh
+++ throttlesym.app/Contents/Resources/throttlesym/bin/throttlesym.sh
@@ -5,7 +5,7 @@ restartCount=0
 
 while :
 do
-	# find the SymDaemon process on each loop
+    # find the SymDaemon process on each loop
     symdPid=`ps -A |grep -m1 SymDaemon |grep -v grep |awk '{print $1}'`
 
     # Formulate the command to contain the processor power Symantec can monopolize
@@ -14,21 +14,21 @@ do
     # important, but it shouldn't take a front seat to actually being able to use the
     # computer, especially when an actual virus is detected... um... barely ever.
     throttleCommand="/usr/local/Cellar/cputhrottle/20100515_1/bin/cputhrottle ${symdPid} 10"
-	
-	# Avoid causing a different problem by preventing a tight restart loop
-	sleep 2s
-	
-	# Output to confirm that the launchagent is running this as root (since it won't work otherwise.)
-	echo "Running as:"
+
+    # Avoid causing a different problem by preventing a tight restart loop
+    sleep 2s
+
+    # Output to confirm that the launchagent is running this as root (since it won't work otherwise.)
+    echo "Running as:"
     whoami
 
     restartCount=$(expr $restartCount + 1)
     echo "cputhrottle has been killed and restarted $restartCount times."
     # Run this in the foreground to block the script until it exits (if it does),
     # and only loop after that.
-	eval ${throttleCommand}
+    eval ${throttleCommand}
 
-	# Update the value of the cputhrottle process (that contains the SymDaemon process).
-	cpuThrottlePid=$!
+    # Update the value of the cputhrottle process (that contains the SymDaemon process).
+    cpuThrottlePid=$!
 
 done
